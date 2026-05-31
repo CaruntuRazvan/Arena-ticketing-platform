3a5c3e12ee5846ff1aab73cc58f221995149583b
Caruntu Razvan <115624498+CaruntuRazvan@users.noreply.github.com>
2026-06-01 00:08:17 +0300

    Add new domain in CORS

--- api-gateway/src/main/java/com/arena/gateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/arena/gateway/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("https://161.35.75.108");
+        config.addAllowedOrigin("https://arena-ticketing.duckdns.org");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
