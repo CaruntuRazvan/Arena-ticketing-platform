4e6dbc327f30786c8434b5b898b53b69619319dc
Caruntu Razvan <115624498+CaruntuRazvan@users.noreply.github.com>
2026-05-31 20:32:05 +0300

    Configure frontend for docker

--- arena-frontend/src/api/axios.js
+++ arena-frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://localhost:8080/api', // portul API GATEWAY
-    baseURL: 'https://localhost/api',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json'
     }
